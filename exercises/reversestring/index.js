a482eaadcd6164dad1ec30b4ee0f0ab6c23c38fd
Fikri Karim <fkfikrikarim@gmail.com>
2018-03-15 18:03:22 +0700

    Reverse da string!

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,33 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// Original solution yay!
+function reverse(str) {
+    var array = str.split('');
+    var reversed = '';
+    for (var i = array.length - 1; i >= 0; i--) {
+        reversed = reversed + array[i];
+    }
+    return reversed;
+}
+
+// Better use this loop, because not too many point for error
+// function reverse(str) {
+//     var reversed = '';
+//     for (let char of str) {
+//         reversed = char + reversed;
+//     }
+//     return reversed;
+// }
+
+// Trivial solution
+// function reverse(str) {
+//     return str.split('').reverse().join('');
+// }
+ 
+// Dunno why error
+// function reverse(str) {
+//     return str.split().reduce((rev, char) => char + rev, '')
+// }
 
 module.exports = reverse;
