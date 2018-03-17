d779573a130b507fe580a5b514e19b1ff1099c98
Fikri Karim <fkfikrikarim@gmail.com>
2018-03-17 16:27:44 +0700

    Alhamdulillah can came up with ma own solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,40 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let total = {};
+    let highest = '';
+    total[highest] = 0;
+
+    // Use
+    // for (const iterator of object) {
+        
+    // }
+    str.split("").forEach((char) => {
+        // console.log(char);
+        
+        // total[char] = total[char] || 1;
+
+        // if (!total[char]) {
+        if (total[char] === undefined) {
+            total[char] = 1
+        } else {
+            // total[char]++
+            total[char] += 1
+        }
+        // console.log(total[char], char);
+        // console.log('foreach');
+        // console.log('highest', highest);
+        // console.log('total[highest]', total[highest]);
+        
+        // Make the checking variable different. Don't use the same total!
+        // For example, use max and maxChar variable
+        if (total[char] > total[highest]) {
+            highest = char
+        }
+    })
+
+    return highest
+}
 
 module.exports = maxChar;
