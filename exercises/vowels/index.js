98da9f4e57fdc19368f8b043f1e9646c4722edcc
Fikri Karim <fkfikrikarim@gmail.com>
2018-03-18 17:39:38 +0700

    vOwEls

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,22 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    let count = 0;
+    let vowelsChars = ['a', 'i', 'u', 'e', 'o']
+
+    for (const char of str) {
+        if (vowelsChars.includes(char.toLowerCase())) {
+            count++
+        }
+    }
+
+    return count
+}
+
+function vowels(str) {
+    array = str.match(/[aiueo]/gi) || []
+    return array.length
+}
 
 module.exports = vowels;
