dc70aaccd2e05723b99eea3ae1f846b209166e34
Fikri Karim <fkfikrikarim@gmail.com>
2018-03-19 16:30:03 +0700

    Alhamdulillah haha

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,46 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n, startIndex = 0, array = [], count = 1) {
+    if (!array[startIndex]) {
+        for (let i = 0; i < n; i++) {
+            array.push([])
+        }
+    }
+    stopIndex = n - startIndex
+    
+    for (let i = startIndex; i < stopIndex; i++) {
+        array[startIndex][i] = count
+        count++
+    }
+
+    for (let i = startIndex + 1; i < stopIndex; i++) {
+        array[i][stopIndex - 1] = count
+        count++
+    }
+
+    for (let i = stopIndex - 1; i > startIndex; i--) {
+        array[stopIndex - 1][i - 1] = count
+        count++
+    }
+
+    for (let i = stopIndex - 1; i > startIndex + 1; i--) {
+        array[i - 1][startIndex] = count
+        count++
+    }
+
+
+    if (startIndex < (n / 2) - 1) {
+        console.log('masuk if' )
+        console.log('n',n);
+        console.log('startIndex',startIndex);
+        
+        return matrix(n, startIndex + 1, array, count)
+    } else {
+        console.log('return');
+        console.log('array', array);
+        return array
+    }
+}
 
 module.exports = matrix;
